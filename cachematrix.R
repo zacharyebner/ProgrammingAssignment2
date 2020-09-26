6d146318b662b35200380c5de81e4639a82fd0ca
zacharyebner <69911408+zacharyebner@users.noreply.github.com>
2020-09-25 22:41:17 -0500

    Update cachematrix.R
    Peer graded assignment #2 commit one

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL  ## this will clear any previous cached value 
+  set <- function(y) { ## this is the 'setter' function 
+    x <<- y     ## the double arrow assignment operator ensures that the x variable inherits the variable from the parent environment
+    m <<- NULL
+  }
+  get <- function() x  ## these are the 'getter' functions 
+  setInverse <- function(solve) inv <<- Inverse ## as per the assingment notes, solve returns an inverse of a matrix
+  getInverse <- function() m 
+  list(set = set, get = get,  ## the list functions allow us to recall the above functions with '$' operators rather than brackets
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -12,4 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         m <- x$getInverse()  ##this will retrive the value of m from above 
+    if(!is.null(m)) {     ## if this evaluates to FALSE then there is no previously stored data for M
+      message("getting cached data")  ## this informs the user of the above
+      return(m)
+    }
+    data <- x$get()  ## this will retrieve matrix from the makeCacheMatrix
+    m <- solve(data, ...)   ## this applies the 'solve' function to get the invirse of the matrix
+    x$setInverse(m)  ## this instruction saves the value in memory 
+    m
+  }
 }
+
